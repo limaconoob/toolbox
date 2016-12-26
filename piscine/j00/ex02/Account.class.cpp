c950c8c6fa4a9608539300f495a11f8435d487f7
Jérémy PEPIN <jpepin@student.42.fr>
2016-12-26 12:58:38 +0100

    ex02 réservé aux attardés

---
+++ piscine/j00/ex02/Account.class.cpp
@@ -0,0 +1,4 @@
+// Sujet écrit dans un sous langage de Fils De Putes réservé aux attardés.
+// Objectif incompréhensible.
+// La prochaine fois faites une dissertation en chinois.
+// Bande de soumis à l'amerique. Connards.
